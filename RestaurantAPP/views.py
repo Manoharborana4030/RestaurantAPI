38a7e6b2406ad44d23724ee6a8795e1a15d70bf4
gautamsinh1234 <gautamsinhmakwana143@gmail.com>
2022-03-10 18:18:13 +0530

    api page

--- RestaurantAPP/views.py
+++ RestaurantAPP/views.py
@@ -8,3 +8,8 @@ from rest_framework.generics import GenericAPIView
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.db.models import Sum
+
+
+
+def home(request):
+    return render(request,'app/home.html')
