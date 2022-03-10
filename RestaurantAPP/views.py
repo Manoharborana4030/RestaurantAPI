e690e274e03fd06cb864729b99b5fcd55a9736fb
arjun <arjundarji9995@outlook.com>
2022-03-10 18:17:35 +0530

    final commit

--- RestaurantAPP/views.py
+++ RestaurantAPP/views.py
@@ -8,3 +8,17 @@ from rest_framework.generics import GenericAPIView
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.db.models import Sum
+
+
+
+class restaurant(APIView):
+	def get(self,request):
+		obj = Restaurant.objects.all()
+		serializer = RestaurantSerializer(obj,many=True)
+		return Response(serializer.data)
+
+class category(APIView):
+	def get(Self,request):
+		obj = Category.objects.all()
+		serializer = CategorySerializer(obj,may=True)
+		return Response(serializer.data)
