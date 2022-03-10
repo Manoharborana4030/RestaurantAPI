8db17c4cb6bfd9ae33da135b8c20749470d41dfe
Manoharborana4030 <19mca050@nirmauni.ac.in>
2022-03-10 18:16:46 +0530

    First Commit
    Where i added Item class

--- RestaurantAPP/views.py
+++ RestaurantAPP/views.py
@@ -8,3 +8,17 @@ from rest_framework.generics import GenericAPIView
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.db.models import Sum
+
+class IteamData(APIView):
+    serializer_class=IteamSSerializer
+    def get(self,request):
+        iteam=Item.objects.all()
+        serializer=IteamSSerializer(iteam,many=True)
+        return Response(serializer.data)
+       
+    def post(self,request):
+        serializer=IteamSSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data Inserted'})
+        return(serializer.errors)
